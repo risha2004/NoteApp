36f598d70ea8b4477a64d251c5d4e25e5baa318b
risha2004 <risha240704@gmail.com>
2024-09-06 14:26:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+//parsers
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// using images,js, css in public folder
+app.use(express.static(path.join(__dirname,'public')));
+
+//  setting view engine
+app.set('view engine', 'ejs');
+
+
+// setting routes
+app.get("/",(req, res)=>{
+    fs.readdir(`./files`,(err, files)=>{
+      // console.log(files);
+       res.render("index",{files: files});
+    })
+    
+});
+
+app.get("/file/:filename",(req, res)=>{
+  fs.readFile(`./files/${req.params.filename}`,"utf-8", (err, filedata)=>{
+        res.render('show', {filename: req.params.filename, filedata:filedata});
+  })
+});
+
+app.get("/edit/:filename",(req, res)=>{
+    res.render('edit',{filename: req.params.filename});
+  });
+
+app.post("/edit", (req, res)=>{
+   fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, (err)=>{
+    res.redirect("/");
+   });
+});  
+
+app.post("/create",(req, res)=>{ 
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err)=>{
+        res.redirect("/");
+    });
+});
+
+
+app.listen(3000, ()=>{
+    console.log("Its running");
+})
